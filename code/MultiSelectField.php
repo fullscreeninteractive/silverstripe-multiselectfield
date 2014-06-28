fdf199e038b481e61edfb3b047aa028dfc305c40
Will Rossiter <will.rossiter@gmail.com>
2014-06-29 10:13:11 +1200

    Add support for saving to a text field, ala checkboxsetfield
    Update to 3.1 syntax

--- code/MultiSelectField.php
+++ code/MultiSelectField.php
@@ -15,27 +15,30 @@ class MultiSelectField extends CheckboxSetField {
 	 *
 	 * @see forms/CheckboxSetField::Field()
 	 */
-	function Field() {
+	function Field($properties = array()) {
 		Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
 		Requirements::javascript(THIRDPARTY_DIR."/jquery-livequery/jquery.livequery.js");
 		Requirements::javascript("multiselectfield/javascript/multiselectfield.js");
 		Requirements::css('multiselectfield/css/multiselectfield.css');
+
 		// Suffix [] to name to allow multiple values
-		$arrayName = $this->Name().'[]';
+		$arrayName = $this->getName().'[]';
 		$attributes = array(
 			'name'=>$arrayName,
 			'id'=>$this->id(),
 			'multiple'=>'multiple',
-			'tabindex'=>$this->getTabIndex(),
-			'class'=>$this->extraClass(),
+			'class'=> $this->extraClass() .' no-chzn',
 		);
-		if ($this->disabled)
-		$attributes['disabled'] = 'disabled';
+		
+		if ($this->disabled) {
+			$attributes['disabled'] = 'disabled';
+		}
 
 		// Build options
 		$content = '';
 		$source = $this->source;
 		$value = $this->getSelected();
+
 		foreach($source as $index => $item) {
 			$selected = (isset($value[$index])) ? 'selected' : '';
 			$content .= "<option $selected value=\"$index\">$item</option>";
@@ -44,44 +47,78 @@ class MultiSelectField extends CheckboxSetField {
 		return $this->createTag('select', $attributes, $content);
 	}
 
+
+	public function Type() {
+		return 'no-chzn multiselect optionset checkboxset';
+	}
+
 	/**
 	 * Save value to dataobject
 	 *
 	 * @see forms/CheckboxSetField::saveInto()
 	 */
-	public function saveInto(DataObject $record) {
-		$fieldName = $this->name;
+	public function saveInto(DataObjectInterface $record) {
+		$fieldName = $this->getName();
 
-		// TODO - SiteTree admin seems to serialise value, whilst modeladmin doesn't
-		// this explodes the serialised string, but there is probably a more elegant solution
 		$valueArray = (isset($this->value[0]) && strpos($this->value[0],',')) ? explode(',',$this->value[0]) : $this->value;
 
 		if ($fieldName && ($record->has_many($fieldName) || $record->many_many($fieldName))) {
 			// Set related records
 			$record->$fieldName()->setByIDList($valueArray);
 		} else {
-			$record->$fieldName = implode(', ',$valueArray);
+			$record->$fieldName = implode(', ', $valueArray);
+			$record->write();
 		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public function dataValue() {
+		return (is_array($this->value)) ? implode(', ', $this->value) : $this->value;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function Value() {
+		return $this->dataValue();
 	}
 
 	/**
 	 * Get array of selected IDs
 	 */
 	public function getSelected() {
-		$value = $this->value;
+		$value = explode(', ', $this->value);
+
 		// If value not set, try to get it from the form
 		if (!$value && is_object($this->form)) {
 			$record = $this->form->getRecord();
-			if ($record && $record->hasMethod($this->name)) {
-				$methodName = $this->name;
-				$join = $record->$methodName();
-				if ($join) {
-					foreach ($join as $joinItem) {
-						$value[] = $joinItem->ID;
+
+			if ($record) {
+				if($record->has_many($fieldName) || $record->many_many($fieldName)) {
+					$methodName = $this->name;
+					$join = $record->$methodName();
+					
+					if ($join) {
+						foreach ($join as $joinItem) {
+							$value[] = $joinItem->ID;
+						}
 					}
 				}
 			}
+		} else {
+			$output = array();
+
+			foreach($value as $k => $v) {
+				if(isset($this->source[$v])) {
+					$output[$v] = $this->source[$v];
+				}
+			}
+
+			return $output;
 		}
+
 		return $value;
 	}
 
@@ -92,6 +129,7 @@ class MultiSelectField extends CheckboxSetField {
 		$items = array();
 		$selected = $this->getSelected();
 		$source = $this->source;
+
 		foreach ($source as $key => $item) {
 			if (!isset($selected[$key])) {
 				$items[$key] = $item;
@@ -113,4 +151,3 @@ class MultiSelectField extends CheckboxSetField {
 	}
 
 }
-?>
